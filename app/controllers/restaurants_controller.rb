2d9ea35ef7f467217f5f311be5800d143c423bd5
Marion Debonnaire <marion.debonnaire@gmail.com>
2020-08-12 15:13:10 +0200

    Exo 2- controllers et routes ok

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,21 @@
 class RestaurantsController < ApplicationController
+
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET /restaurants/1
+  def show
+    @review = Review.new
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # GET /restaurants/1/edit
+  def edit
+  end
 end
