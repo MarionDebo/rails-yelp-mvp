bfd88791937628a1caaa34d00eabdf4950bf8975
Marion Debonnaire <marion.debonnaire@gmail.com>
2020-08-12 16:21:14 +0200

    Working on the views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,59 +1,57 @@
 class RestaurantsController < ApplicationController
-
+  # before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
   # GET /restaurants
   def index
     @restaurants = Restaurant.all
   end
 
-  # GET /restaurants/1
+# #   # GET /restaurants/1
   def show
     @review = Review.new
   end
 
-  # GET /restaurants/new
-  def new
-    @restaurant = Restaurant.new
-  end
+# #   # GET /restaurants/new
+#   def new
+#     @restaurant = Restaurant.new
+#   end
 
-  # GET /restaurants/1/edit
-  def edit
-  end
+# #   # GET /restaurants/1/edit
+#   def edit
+#   end
 
-  # POST /restaurants
-  def create
-    @restaurant = Restaurant.new(restaurant_params)
+#   # POST /restaurants
+#   def create
+#     @restaurant = Restaurant.new(restaurant_params)
 
-    if @restaurant.save
-      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
-    else
-      render :new
-    end
-  end
+#     if @restaurant.save
+#       redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+#     else
+#       render :new
+#     end
+#   end
 
-  # PATCH/PUT /restaurants/1
-  def update
-    if @restaurant.update(restaurant_params)
-      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
-    else
-      render :edit
-    end
-  end
+#   # PATCH/PUT /restaurants/1
+#   def update
+#     if @restaurant.update(restaurant_params)
+#       redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+#     else
+#       render :edit
+#     end
+#   end
 
-  # DELETE /restaurants/1
-  def destroy
-    @restaurant.destroy
-    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
-  end
+#   # DELETE /restaurants/1
+#   def destroy
+#     @restaurant.destroy
+#     redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
+#   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
+#   private
+#     # Use callbacks to share common setup or constraints between actions.
+\
 
-    # Only allow a trusted parameter "white list" through.
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :stars)
-    end
+#     # Only allow a trusted parameter "white list" through.
+#     def restaurant_params
+#       params.require(:restaurant).permit(:name, :address, :stars)
+#     end
 
 end
